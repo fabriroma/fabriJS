354d9d8b8714a37696fd968b9b907335a65264fb
fabrizio <fabrizio.roman1979@gmail.com>
2023-10-14 22:40:04 -0300

    agregue el dom sin el prompt y el alert

--- app.js
+++ app.js
@@ -1,94 +1,78 @@
-
-function saludar() {
-    let iniciar = "";
-
-    while (iniciar.toLowerCase() !== "ok") {
-        iniciar = prompt("¡Bienvenido a nuestra tienda de ropa en línea! Ingrese 'ok' para comenzar:").toLowerCase();
-    }
-}
-
-function comprarRopa() {
-    let carrito = [];
-    let continuarComprando = "si";
-
-    while (continuarComprando === "si") {
-        let producto = prompt("Seleccione un producto:\n1- Camisa\n2- Pantalón\n3- Zapatos\n4- Chaqueta");
-
-        switch (producto) {
-            case "1":
-                carrito.push({ nombre: "Camisa", precio: 20 });
-                break;
-            case "2":
-                carrito.push({ nombre: "Pantalón", precio: 30 });
-                break;
-            case "3":
-                carrito.push({ nombre: "Zapatos", precio: 50 });
-                break;
-            case "4":
-                carrito.push({ nombre: "Chaqueta", precio: 40 });
-                break;
-            default:
-                alert("Opción no válida. Por favor, seleccione un producto válido.");
-        }
-
-        continuarComprando = prompt("¿Desea agregar más productos al carrito? (si/no)").toLowerCase();
-    }
-
-    return carrito;
-}
-
-function calcularTotal(carrito) {
-    let total = 0;
-
-    for (let i = 0; i < carrito.length; i++) {
-        total += carrito[i].precio;
-    }
-
-    return total;
-}
-
-function actualizarContadorCarrito() {
-    const carritoGuardado = localStorage.getItem("carrito");
-    const carrito = carritoGuardado ? JSON.parse(carritoGuardado) : [];
-    contadorCarrito.textContent = carrito.length;
-}
-
-function limpiarCarrito() {
-    localStorage.removeItem("carrito");
-    actualizarContadorCarrito();
-    mensajeElement.textContent = "El carrito ha sido limpiado.";
-}
-
-function tiendaDeRopa() {
-    saludar();
-    let carrito = comprarRopa();
-
-    if (carrito.length === 0) {
-        alert("No ha seleccionado ningún producto. Gracias por visitarnos.");
-    } else {
-        alert("Productos seleccionados: ");
-        for (let i = 0; i < carrito.length; i++) {
-            alert(`${carrito[i].nombre} - Precio: $${carrito[i].precio}`);
-        }
-
-        let total = calcularTotal(carrito);
-        alert(`El monto total de su compra es de $${total}. ¡Gracias por comprar en nuestra tienda de ropa en línea!`);
-    }
-
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+document.addEventListener("DOMContentLoaded", function () {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
     const contadorCarrito = document.getElementById("contador-carrito");
+    const galeria = document.querySelector(".galeria");
+    const mensaje = document.getElementById("mensaje");
     const limpiarCarritoBtn = document.getElementById("limpiar-carrito");
-    const mensajeElement = document.getElementById("mensaje");
 
+    galeria.addEventListener("click", function (event) {
+      if (event.target.classList.contains("btn")) {
+        const producto = event.target.parentElement.querySelector(".product-name").textContent;
+        const precio = parseFloat(event.target.parentElement.querySelector(".product-price").textContent.replace("$", ""));
+        agregarProductoAlCarrito(producto, precio);
+        mostrarMensaje(`"${producto}" agregado al carrito`);
+      }
+    });
 
-    limpiarCarritoBtn.addEventListener("click", limpiarCarrito);
+    limpiarCarritoBtn.addEventListener("click", function () {
+      carrito.length = 0; 
+      actualizarCarrito();
+      mostrarMensaje("Carrito limpiado");
+    });
 
+    function agregarProductoAlCarrito(nombre, precio) {
+      const productoEnCarrito = carrito.find((item) => item.nombre === nombre);
+      if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+      } else {
+        carrito.push({ nombre, precio, cantidad: 1 });
+      }
 
-    actualizarContadorCarrito();
-}
+      actualizarCarrito();
+    }
 
-tiendaDeRopa();
+    function actualizarCarrito() {
+      const mensajesDiv = document.getElementById("mensajes");
+      mensajesDiv.innerHTML = "";
+      let totalCarrito = 0; 
 
+      contadorCarrito.textContent = carrito.reduce((total, item) => total + item.cantidad, 0);
+
+      carrito.forEach((item) => {
+        const itemHTML = document.createElement("div");
+        itemHTML.classList.add("carrito-item");
+        itemHTML.innerHTML = `
+            <span class="carrito-nombre">${item.nombre}</span>
+            <div class="carrito-precio">Precio Unitario: $${item.precio.toFixed(2)}</div>
+            <div class="carrito-cantidad">Cantidad: ${item.cantidad}</div>
+            <div class="carrito-cantidad">Total ${item.nombre}: $${item.precio.toFixed(2)*item.cantidad}</div>
+        `;
+        mensajesDiv.appendChild(itemHTML);
+
+        const subtotal = item.precio * item.cantidad;
+        totalCarrito += subtotal;
+      });
+
+      const totalCarritoElement = document.createElement("div");
+      totalCarritoElement.classList.add("carrito-total");
+      totalCarritoElement.innerHTML = `
+        <span>Total General:</span>
+        <span class="carrito-precio">$${totalCarrito.toFixed(2)}</span>
+      `;
+      mensajesDiv.appendChild(totalCarritoElement);
+
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+
+    function mostrarMensaje(mensajeTexto) {
+      mensaje.textContent = mensajeTexto;
+      setTimeout(() => {
+        mensaje.textContent = "";
+      }, 3000);
+    }
+
+    actualizarCarrito();
+  });
 
 
